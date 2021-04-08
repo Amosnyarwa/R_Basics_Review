87a8a278392b66d31a1fab405822426a7da22fbf
LAP095\galen <amos.nyarwaya@gmail.com>
2021-04-08 17:06:51 +0300

    Added vectors

--- R/R_basics.R
+++ R/R_basics.R
@@ -2,3 +2,11 @@
 
 my_first_String <- "hello world"
 print(my_first_String)
+
+# Data structures
+# vectors
+fruits <- c("banana", "oranges", "mangoes")
+print(fruits)
+
+some_numbers <- c(1:10)
+print(some_numbers)
