0866cf869f146654329f6138d3efbe10778bf4fe
LAP095\galen <amos.nyarwaya@gmail.com>
2021-04-09 17:02:26 +0300

    Looked at data structures like lists, factors, data frames, matrices and factors

--- R/R_basics.R
+++ R/R_basics.R
@@ -23,5 +23,48 @@ print(my_list)
 
 my_list
 
-# Matrices - two dimensional rectangular data set
+# Matrices - two dimensional rectangular data 
+
+my_matrix <- matrix(some_numbers, nrow = 5, ncol = 10, byrow = T)
+my_matrix
+
+# Array
+
+my_array <- array(fruits, dim = c(3,3,4))
+my_array
+
+# Factors
+
+my_colours <- c("red", "green", "red", "blue", "red", "green", "red", "blue")
+
+# Check to see if it is a factor
+is.factor(my_colours)
+my_colours_factor <- factor(my_colours)
+my_colours_factor
+
+# Check to see if it is a factor
+is.factor(my_colours_factor)
+
+# Data frame
+# These are tabular data objects
+# Have different data types
+
+my_dataframe <- data.frame(gender = c("Male", "Male","Female"), 
+                           height = c(152, 171.5, 165), 
+                           weight = c(81,93, 78),
+                           Age = c(42,38,26))
+my_dataframe
+
+class(my_dataframe)
+
+
+
+
+
+
+
+
+
+
+
 
