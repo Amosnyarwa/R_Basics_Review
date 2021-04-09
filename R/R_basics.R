29946262cea3b707405a4e422d2b73e0b254f443
LAP095\galen <amos.nyarwaya@gmail.com>
2021-04-09 16:30:13 +0300

    More data structures

--- R/R_basics.R
+++ R/R_basics.R
@@ -10,3 +10,18 @@ print(fruits)
 
 some_numbers <- c(1:10)
 print(some_numbers)
+
+# Check data types using class function
+
+class(fruits)
+class(some_numbers)
+
+# List
+
+my_list <- list(c(2,5,3),21.3,sin)
+print(my_list)
+
+my_list
+
+# Matrices - two dimensional rectangular data set
+
