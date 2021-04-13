ed27ae9d00f946037950025e62206768a231dfaf
LAP095\galen <amos.nyarwaya@gmail.com>
2021-04-13 17:18:41 +0300

    Some useful functions and operators

--- R/R_basics.R
+++ R/R_basics.R
@@ -4,7 +4,7 @@ my_first_String <- "hello world"
 print(my_first_String)
 
 # Data structures
-# vectors
+# vectors - one dimensional array (when you want to create a vector with more than one element, you should use c()function to combine the elements into a vector.
 fruits <- c("banana", "oranges", "mangoes")
 print(fruits)
 
@@ -16,24 +16,24 @@ print(some_numbers)
 class(fruits)
 class(some_numbers)
 
-# List
+# List - Is an R-object which can contain many different types of elements inside it like vectors, functions and even another list inside it.
 
 my_list <- list(c(2,5,3),21.3,sin)
 print(my_list)
 
 my_list
 
-# Matrices - two dimensional rectangular data 
+# Matrices - Is data represented in a two dimensional rectangular data 
 
 my_matrix <- matrix(some_numbers, nrow = 5, ncol = 10, byrow = T)
 my_matrix
 
-# Array
+# Array - can be of any dimensions.
 
 my_array <- array(fruits, dim = c(3,3,4))
 my_array
 
-# Factors
+# Factors - are data objects used for the purpose of categorizing data and then storing them under levels.
 
 my_colours <- c("red", "green", "red", "blue", "red", "green", "red", "blue")
 
@@ -57,13 +57,64 @@ my_dataframe
 
 class(my_dataframe)
 
+# Some useful functions
+# Paste combines strings
+my_name <- "amos"
+my_name_string <- paste("my name is:", my_name)
+print(my_name_string)
+
+# Runif
+my_random_numbers <- runif(20, 100, 230)
+print(my_random_numbers)
+?runif
+
+ #Rep-repeating vectors
+my_repeat <- rep(fruits, each = 5)
+print(my_repeat)
+
+# Mathematical operators
+# Arithmetic operators These are the types of operators used to perform mathematicaloperations like, addition, subtraction, division
+# + :Addition
+# -:Subtraction
+# * :Multiplication
+# / :Division
+# ^ :Exponent
+# %% :Modulus (Remainder from division)
+# %/% :Integer Division
+
+# Relational operators- help us make comparisons between values.
+# < :Less than
+# > :Greater than
+# <= :Less than or equal to
+# >= :Greater than or equal to
+# == :Equal to
+# != :Not equal to
+
+# Logical operators - We use these operators to perform Boolean operations such as AND, OR etc. Logical operators are only applicable to numeric, logical or complex vectors.
+# !: Logical NOT
+# & :Element-wise logical AND
+# && :Logical AND
+# | :Element-wise logical OR
+# || :Logical OR
+
+# Assignment operators - We use assignment operators to assign values to the variables.
+# <-, <<-, = :Leftwards assignment
+# ->, ->> :Rightwards assignment
 
 
+if(my_name %in% my_name_string){
+  print(my_name)
+  
+  if ("name" %in% my_name_string) {
+    print("test passed")
+  }
+}
 
-
-
-
-
+if(my_name %in% my_name_string){
+  print(paste("my name is:", my_name))
+}else{
+  print(paste("my name: ", my_name, "is not in the string"))
+}
 
 
 
